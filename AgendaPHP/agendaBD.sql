c41d870789b30638751ddbc5e422119af536e99d
jhonlagos1994 <jhon.lagos1994@gmail.com>
2019-08-19 20:49:43 -0500

    Interactuando con Bases de Datos

---
+++ AgendaPHP/agendaBD.sql
@@ -0,0 +1,86 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `agenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `fechainicio` date NOT NULL,
+  `horainicio` time NOT NULL,
+  `fechafin` date NOT NULL,
+  `horafin` time NOT NULL,
+  `idusuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `evento`
+--
+
+INSERT INTO `evento` (`id`, `title`, `fechainicio`, `horainicio`, `fechafin`, `horafin`, `idusuario`) VALUES
+(7, 'Daniel', '2019-08-06', '05:30:00', '2019-08-06', '06:30:00', 1),
+(10, 'Prueba secu', '2019-08-08', '07:00:00', '0000-00-00', '00:00:00', 1),
+(11, 'Prueba secoifa', '2019-08-02', '07:00:00', '2019-08-02', '07:00:00', 1),
+(13, 'Carlos ', '2019-08-07', '05:00:00', '2019-08-07', '06:00:00', 2),
+(14, 'confirmacion', '2019-08-11', '07:00:00', '2019-08-11', '07:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `psw` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `psw`, `email`) VALUES
+(1, 'John Lagos', 'Scoforum1997.', 'jhon.lagos1994@gmail.com'),
+(2, 'Daniel Fierro ', 'Scoforum1997.', 'jhon.lagos1994@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
